08010074fea9cdba26f7b182ebccc3ee091a1ff5
Arthur <chupryna.2004@mail.ru>
2025-02-27 00:04:46 +0500

    added call class Car

--- src/Program.java
+++ src/Program.java
@@ -4,21 +4,8 @@ public class Program {
 
     public static void main (String[] args){
 
-        Scanner in = new Scanner(System.in);
-        int n = 0;
+        Car car = new Car();
+        car.drive();
 
-        do
-        {
-            System.out.print("Введите первое число: ");
-            int num1 = in.nextInt();
-            System.out.print("Введите второе число: ");
-            int num2 = in.nextInt();
-            System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
-
-            System.out.println("Для выхода из программы нажмите цифру 1, для подолжения любую другую цифру");
-            n = in.nextInt();
-        }
-        while (n != 1);
-        in.close();
     }
 }
