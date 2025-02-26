0cc0a19b6ed5393d4eb269c63e816497185b03de
Arthur <chupryna.2004@mail.ru>
2025-02-27 00:05:31 +0500

    added classes for the Car class

---
+++ src/Car.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Wheel {
+    void drive() {
+        System.out.println("Колеса едут");
+    }
+}
+
+class Engine {
+    void drive() {
+        System.out.println("Двигатель запущен");
+    }
+}
+
+class Car {
+    private Engine engine;
+    private List<Wheel> wheels;
+
+    // Композиция
+    Car() {
+        this.engine = new Engine();
+        this.wheels = new ArrayList<>();
+        this.wheels.add(new Wheel()); // Добавляем одно колесо
+        this.wheels.add(new Wheel()); // Добавляем второе колесо
+        this.wheels.add(new Wheel()); // Добавляем третье колесо
+        this.wheels.add(new Wheel()); // Добавляем четвертое колесо
+    }
+
+    // Делегирование
+    void drive() {
+        this.engine.drive();
+        for (Wheel wheel : this.wheels) {
+            wheel.drive();
+        }
+    }
+}
