70459136c990ce69cc26f3c58c20077d51782d0e
Arthur <chupryna.2004@mail.ru>
2025-02-28 20:28:56 +0500

    added class Person & added attributes to class

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,61 @@
-public class Program {
+public class Program{
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        Car car = new Car();
-        car.drive();
+        Person kate = new Person("Катя", 30, "Женский");
+        kate.displayInfo();
+        System.out.println(kate.getName());
+        System.out.println(kate.getAge());
+        System.out.println(kate.getGender());
 
+        Person misha = new Person("Миша", 27, "Мужской");
+        misha.displayInfo();
+        System.out.println(misha.getName());
+        System.out.println(misha.getAge());
+        System.out.println(misha.getGender());
+    }
+}
+class Person {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    void displayInfo() {
+        setGender(gender);
+        setName(name);
+        setAge(age);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Неверный возраст");
+        } this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 }
