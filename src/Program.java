3768db20f0e014ed5a405333d96481281716d56b
Arthur <chupryna.2004@mail.ru>
2025-03-01 00:20:54 +0500

    added call class BankAccount

--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,17 @@ public class Program{
 
     public static void main(String[] args) {
 
+        BankAccount acc1 = new BankAccount("1", 35000, "Иван Иванов");
+        acc1.displayBalance();
+        acc1.deposit(10000);
+        acc1.displayBalance();
+        // Попытка снять больше, чем есть на балансе
+        acc1.withdraw(50000);
+        acc1.displayBalance();
+
+
+
+        /* class Person
         Person kate = new Person("Катя", 30, "Женский");
         kate.displayInfo();
         System.out.println(kate.getName());
@@ -13,5 +24,6 @@ public class Program{
         System.out.println(misha.getName());
         System.out.println(misha.getAge());
         System.out.println(misha.getGender());
+         */
     }
 }
