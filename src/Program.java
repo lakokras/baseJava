9b496c9f42f58001caafe03c128abe745440f2c1
Arthur <chupryna.2004@mail.ru>
2025-03-01 22:56:53 +0500

    minor fixes

--- src/Program.java
+++ src/Program.java
@@ -2,28 +2,11 @@ public class Program{
 
     public static void main(String[] args) {
 
-        BankAccount acc1 = new BankAccount("1", 35000, "Иван Иванов");
-        acc1.displayBalance();
-        acc1.deposit(10000);
-        acc1.displayBalance();
-        // Попытка снять больше, чем есть на балансе
-        acc1.withdraw(50000);
-        acc1.displayBalance();
-
-
-
-        /* class Person
-        Person kate = new Person("Катя", 30, "Женский");
-        kate.displayInfo();
-        System.out.println(kate.getName());
-        System.out.println(kate.getAge());
-        System.out.println(kate.getGender());
-
-        Person misha = new Person("Миша", 27, "Мужской");
-        misha.displayInfo();
-        System.out.println(misha.getName());
-        System.out.println(misha.getAge());
-        System.out.println(misha.getGender());
-         */
+        BankAccount account = new BankAccount("Катя", 30, "1", 35000);
+        account.displayAccountBalance();
+        account.deposit(5000);
+        account.displayAccountBalance();
+        account.withdraw(1000);
+        account.displayAccountBalance();
     }
 }
