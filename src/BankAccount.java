7a4e7c73d98147a946fe68de6b8a014fbee583fb
Arthur <chupryna.2004@mail.ru>
2025-03-02 19:58:47 +0500

    minor fixes

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,32 +1,32 @@
-public class BankAccount extends Human {
+public class BankAccount {
 
     private final String accountNumber;
     private double balance;
+    private String owner;
 
 
-    public BankAccount(String name, int age, String accountNumber, double balance) {
-        super(name, age);
+    public BankAccount(String accountNumber, double balance, String owner) {
         this.accountNumber = accountNumber;
         this.balance = balance;
+        this.owner = owner;
     }
 
-    public void displayAccountBalance() {
-        displayInfo();
-        System.out.printf("Ваш текущий баланс: %.2f \n", balance);
+    public void displayAccountInfo() {
+        System.out.printf(" Ваш текущий баланс: %.2f \n", balance);
     }
 
     public void deposit(double amount) {
         if (amount < 0) {
             throw new IllegalArgumentException("Сумма пополнения не может быть отрицательным числом");
         } this.balance += amount;
-        System.out.printf("Начислена сумма %.2f \n", amount);
+        System.out.printf("Начислена сумма %.2f | Текущий баланс: %.2f \n", amount, balance);
     }
 
     public void withdraw(double amount) {
         if (amount > balance) {
             throw new IllegalArgumentException("Сумма снятия не может превышать текущий баланс");
         } this.balance -= amount;
-        System.out.printf("Списана сумма %.2f \n", amount);
+        System.out.printf("Списана сумма %.2f | Текущий баланс: %.2f \n", amount, balance);
     }
 
     public String getAccountNumber() {
