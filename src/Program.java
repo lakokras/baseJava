aa38059e2a5ce529518694d80904603a44f22348
Arthur <chupryna.2004@mail.ru>
2025-03-06 18:04:03 +0500

    minor add

--- src/Program.java
+++ src/Program.java
@@ -1,13 +1,24 @@
 public class Program{
 
     public static void main(String[] args) {
-        Human person1 = new Human("Джейн", 21);
-        BankAccount account1 = new BankAccount("1", 50000, person1.getName());
-        Car car1 = new Car("BMW", "2010", 0, person1.getName());
+
+        BankClient client1 = new BankClient("Михаил", 30);
+        BankAccount account1 = new BankAccount("1", 50000);
+
+        client1.displayInfo();
+        account1.displayAccountInfo();
+        // методы класса BankAccount
+        account1.deposit(10000);
+        account1.withdraw(15000);
+
+
+        Human person1 = new Human("Даниил", 27);
+        Car car1 = new Car("BMW", "2010", 0);
 
         person1.displayInfo();
-        account1.displayAccountInfo();
         car1.displayCarInfo();
-
+        // методы класса Car
+        car1.accelerate(70);
+        car1.brake(20);
     }
 }
