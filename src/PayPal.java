a6c92d9ffde2b10abbe0e55d1cd0ee0641a3c3b0
Arthur <chupryna.2004@mail.ru>
2025-03-10 02:30:56 +0500

    added class PayPal & redefined pay method

---
+++ src/PayPal.java
@@ -0,0 +1,24 @@
+public class PayPal implements Payable {
+
+    private String email;
+
+    public PayPal(String email) {
+        setEmail(email);
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.printf("Ваш емаил: %s ", email);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (null == email || !email.contains("@") || !email.contains(".")) {
+            throw new IllegalArgumentException("Афигел писать емаил без собаки и точки!?");
+        }
+        this.email = email;
+    }
+}
