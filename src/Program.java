3058bcdddda152b79a64a912d9e9fe8049b3f701
Arthur <chupryna.2004@mail.ru>
2025-03-10 02:23:45 +0500

    minor changes

--- src/Program.java
+++ src/Program.java
@@ -2,23 +2,39 @@ public class Program{
 
     public static void main(String[] args) {
 
-        BankClient client1 = new BankClient("Михаил", 30);
-        BankAccount account1 = new BankAccount("1", 50000);
 
-        client1.displayInfo();
-        account1.displayAccountInfo();
-        // методы класса BankAccount
-        account1.deposit(10000);
-        account1.withdraw(15000);
+        Payable[] payableMethods = new Payable[3];
+        payableMethods[0] = new CreditCard("1234-5678-9123-4567");
+        payableMethods[1] = new PayPal("example@gmail.com");
+        payableMethods[2] = new BankTransfer("109249071");
+
+        for (Payable payable : payableMethods) {
+            payable.pay(100);
+        }
 
 
-        CarOwner owner = new CarOwner("Даниил", 27);
-        Car car1 = new Car("BMW", "2010", 0);
 
-        owner.displayInfo();
-        car1.displayCarInfo();
-        // методы класса Car
-        car1.accelerate(70);
-        car1.brake(20);
+
+
+//        Employee manager = new Manager("Апероль", 12, 120000, "IT");
+//        Employee developer = new Developer("Айзат", 11, 70000, "PHP");
+//
+//        manager.work();
+//        manager.displayEmployeeInfo();
+//
+//        developer.work();
+//        developer.displayEmployeeInfo();
+
+
+//
+//
+//        CarOwner owner = new CarOwner("Даниил", 27);
+//        Car car1 = new Car("BMW", "2010", 0);
+//
+//        owner.displayInfo();
+//        car1.displayCarInfo();
+//        // методы класса Car
+//        car1.accelerate(70);
+//        car1.brake(20);
     }
 }
