4e322e8b10c51484705c27f33b9c32b4f1d6ddce
Arthur <chupryna.2004@mail.ru>
2025-03-20 19:02:45 +0500

    create a separate class Developer

---
+++ src/Developer.java
@@ -0,0 +1,22 @@
+class Developer extends Employee {
+    private String programmingLanguage;
+
+
+    Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void work() {
+        System.out.println("Работает " + getName() + " использует " + getProgrammingLanguage());
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+}
