5872a4e56075f464cc6b89f6e909bd5170aee2b5
Arthur <chupryna.2004@mail.ru>
2025-03-20 19:03:02 +0500

    minor fixes

--- src/Program.java
+++ src/Program.java
@@ -1,40 +1,13 @@
 public class Program{
 
     public static void main(String[] args) {
+        Employee manager = new Manager("Апероль", 12, 120000, "IT");
+        Employee developer = new Developer("Айзат", 11, 70000, "PHP");
 
+        manager.work();
+        manager.displayEmployeeInfo();
 
-        Payable[] payableMethods = new Payable[3];
-        payableMethods[0] = new CreditCard("1234-5678-9123-4567");
-        payableMethods[1] = new PayPal("example@gmail.com");
-        payableMethods[2] = new BankTransfer("109249071");
-
-        for (Payable payable : payableMethods) {
-            payable.pay(100);
-        }
-
-
-
-
-
-//        Employee manager = new Manager("Апероль", 12, 120000, "IT");
-//        Employee developer = new Developer("Айзат", 11, 70000, "PHP");
-//
-//        manager.work();
-//        manager.displayEmployeeInfo();
-//
-//        developer.work();
-//        developer.displayEmployeeInfo();
-
-
-//
-//
-//        CarOwner owner = new CarOwner("Даниил", 27);
-//        Car car1 = new Car("BMW", "2010", 0);
-//
-//        owner.displayInfo();
-//        car1.displayCarInfo();
-//        // методы класса Car
-//        car1.accelerate(70);
-//        car1.brake(20);
+        developer.work();
+        developer.displayEmployeeInfo();
     }
 }
