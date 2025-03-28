551bdf632329149e335ed89f9b5496f4bf987431
Arthur <chupryna.2004@mail.ru>
2025-03-28 18:24:53 +0500

    add class SavingsAccount

---
+++ src/SavingsAccount.java
@@ -0,0 +1,19 @@
+public class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    public SavingsAccount(int accountNumber, double balance, String name, double interestRate) {
+        super(accountNumber, balance, name);
+        setInterestRate(interestRate);
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        if (interestRate <= 0) {
+            throw new IllegalArgumentException("Процент не может быть нулем или отрицательным число");
+        }
+        this.interestRate = interestRate;
+    }
+}
