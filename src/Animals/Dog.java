8eb89d2d204e704c696c26173f90d8eb3bd790dd
Arthur <chupryna.2004@mail.ru>
2025-03-28 18:30:14 +0500

    add class Dog

---
+++ src/Animals/Dog.java
@@ -0,0 +1,13 @@
+package Animals;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Гав");
+    }
+}
