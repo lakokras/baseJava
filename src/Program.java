b0ce166c76583dcaf5e7232f3bcb8c9f94d4c9dc
Arthur <chupryna.2004@mail.ru>
2026-05-02 01:59:09 +0500

    add call to acc1 and acc2 using toString() method

--- src/Program.java
+++ src/Program.java
@@ -13,23 +13,25 @@ public class Program{
         BankAccount acc1 = new BankAccount("ACC001", 1000, human1.getName());
         SavingsAccount acc2 = new SavingsAccount("SAV001", 5000, 5.0, human2.getName());
 
-        List<BankAccount> bankAccounts = new ArrayList<>();
-        bankAccounts.add(acc1);
-        List<SavingsAccount> savingsAccounts = new ArrayList<>();
-        savingsAccounts.add(acc2);
+//        List<BankAccount> bankAccounts = new ArrayList<>();
+//        bankAccounts.add(acc1);
+//        List<SavingsAccount> savingsAccounts = new ArrayList<>();
+//        savingsAccounts.add(acc2);
 
-        Bank bank = new Bank(bankAccounts, savingsAccounts);
+//        Bank bank = new Bank(bankAccounts, savingsAccounts);
 
-        try {
-            bank.transfer("ACC001", "SAV001", 500);
-        } catch (AccountNotFoundException | InsufficientFundsException | InvalidAccountException |
-                 InvalidAmountException e) {
-            System.err.println("Ошибка перевода: " + e.getMessage());
-        }
+//        try {
+//            bank.transfer("ACC001", "SAV001", 500);
+//        } catch (AccountNotFoundException | InsufficientFundsException | InvalidAccountException |
+//                 InvalidAmountException e) {
+//            System.err.println("Ошибка перевода: " + e.getMessage());
+//        }
 
 //        human1.humanName();
 //        human2.humanName();
-        acc1.displayBalance();
-        acc2.displayBalance();
+//        acc1.displayBalance();
+//        acc2.displayBalance();
+        System.out.println(acc1);
+        System.out.println(acc2);
     }
 }
